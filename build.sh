9f2eb1ff1ab487d1e880069d842f16d8d3606c84
Matthew Bauer <mjbauer95@gmail.com>
2015-11-04 21:41:41 -0600

    fix another stupid mistake.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 jspm bundle-sfx index.coffee!
-zip build.zip index.html build.js google-analytics-bundle.js index.css manifest.json background.js
+zip -r build.zip index.html build.js google-analytics-bundle.js index.css manifest.json background.js icon/
