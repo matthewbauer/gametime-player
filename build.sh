5dce96ada0653c068a143c382d773a185bedb386
Matthew Bauer <mjbauer95@gmail.com>
2015-11-04 21:03:55 -0600

    Fix build script.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 jspm bundle-sfx index.coffee!
-zip -r build.zip . -x '*.git*' -x '*jspm_packages*' -x '*node_modules*' -x 'build.zip'
+zip build.zip index.html build.js google-analytics-bundle.js index.css manifest.json
